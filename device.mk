5b8c6c99880d903705900f709bfcdcea6cddb916
TheScarastic <warabhishek@gmail.com>
2017-07-24 20:05:14 +0530

    mido: Kang blobs from oxygen 7.1.1 release
     * Xiaomi/oxygen/oxygen:7.1.1/NMF26F/7.7.13:user/release-keys
    
    Change-Id: I4c51f630cb5f08b9c598923f834eb4335e3ff7bc

--- device.mk
+++ device.mk
@@ -176,10 +176,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# Libshims
-PRODUCT_PACKAGES += \
-    libshims_ims
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8953
